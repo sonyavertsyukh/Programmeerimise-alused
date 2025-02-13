9825528e29f6c092878c795c07bd028f5599ae70
sonyavertsyukh <sonyavertsyukh@gmail.com>
2025-02-13 15:20:37 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import emsine as esimeneFail
+import teine as teineFail
+import kolmas as kolmasFail
+
+userInput = input("Milline ülesanne sa tahad ülevaadata")
+if userInput == "1":
+    esimeneFail.myFunc()
+elif userInput == "2":
+    teineFail.myPykkar()
+else:
+    pass
