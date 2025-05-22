f75171b9b3268a8b58b883424f27aef22f253cdf
sonyavertsyukh <sonyavertsyukh@gmail.com>
2025-05-22 09:33:59 +0300

    Add files via upload

---
+++ rabota n31.py
@@ -0,0 +1,12 @@
+class MurderStatsManager:
+    def __init__(self):
+        self.data = {}
+
+    def add_data(self, continent, country, city, count):
+        if continent not in self.data:
+            self.data[continent] = {}
+        if country not in self.data[continent]:
+            self.data[continent][country] = {}
+        self.data[continent][country][city] = count
+
+ 
