393ad7807f8a31a45f74a22a91159ffdd87dba4f
sonyavertsyukh <sonyavertsyukh@gmail.com>
2025-05-22 09:34:44 +0300

    Update rabota n3.2.py

--- rabota n31.py
+++ rabota n31.py
@@ -9,4 +9,14 @@ class MurderStatsManager:
             self.data[continent][country] = {}
         self.data[continent][country][city] = count
 
+    def get_stats(self):
+        return self.data
+if __name__ == "__main__":
+    manager = MurderStatsManager()
+    manager.add_data("Europe", "Estonia", "Tallinn", 5)
+    manager.add_data("Europe", "Finland", "Helsinki", 3)
+    manager.add_data("Asia", "Japan", "Tokyo", 8)
+
+    print(manager.get_stats())
+
  
